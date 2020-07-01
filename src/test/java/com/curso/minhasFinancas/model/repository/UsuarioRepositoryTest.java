a9d3ea5ca9cf613da9b0b1376af585af46942af7
Mateus Selani <selaneitor@gmail.com>
2020-07-01 11:01:58 -0300

    Adicionando banco de testes e testes unitarios

--- src/test/java/com/curso/minhasFinancas/model/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/curso/minhasFinancas/model/repository/UsuarioRepositoryTest.java
@@ -1,16 +1,18 @@
 package com.curso.minhasFinancas.model.repository;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.curso.minhasFinancas.model.entity.Usuario;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
 public class UsuarioRepositoryTest {
 
 	@Autowired
@@ -23,10 +25,23 @@ public class UsuarioRepositoryTest {
 		Usuario usuario = Usuario.builder().nome("usuario").email("teste@email.com").build();
 		repository.save(usuario);
 
-		// Ação
+		// Ação // Execução
 		boolean result = repository.existsByEmail("teste@email.com");
 
 		// Verificação
-		Assertions.assertThat(result).isTrue();
+		// Assertions.assertThat(result).isTrue();
+		Assertions.assertEquals(true, result);
+	}
+	
+	@Test
+	private void deveRetornarFalsoUsuarioSemEmail() {
+		// Cenario
+		repository.deleteAll();
+		
+		//Ação
+		boolean result = repository.existsByEmail("teste@email.com");
+		
+		//Verificação
+		Assertions.assertEquals(false, result);
 	}
 }
