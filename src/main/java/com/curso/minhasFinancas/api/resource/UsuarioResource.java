40c108f0f2a991bfa00a7f2ff37f59df54209a4c
Mateus Selani <selaneitor@gmail.com>
2020-07-02 22:26:46 -0300

    Adicionando rotas

--- src/main/java/com/curso/minhasFinancas/api/resource/UsuarioResource.java
+++ src/main/java/com/curso/minhasFinancas/api/resource/UsuarioResource.java
@@ -1,8 +1,11 @@
 package com.curso.minhasFinancas.api.resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +20,8 @@ public class UsuarioResource {
 
 	private UsuarioService service;
 
-	public UsuarioResource(UsuarioService service) {
+	public UsuarioResource(@RequestBody UsuarioService service) {
+		super();
 		this.service = service;
 	}
 
