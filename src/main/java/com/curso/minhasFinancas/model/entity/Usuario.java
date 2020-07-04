8737be0e641d9d0603efb2f7401c9fda061ee06b
Mateus Selani <selaneitor@gmail.com>
2020-07-04 10:19:51 -0300

    Corrigindo entidade Usuario

--- src/main/java/com/curso/minhasFinancas/model/entity/Usuario.java
+++ src/main/java/com/curso/minhasFinancas/model/entity/Usuario.java
@@ -7,15 +7,22 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "usuario", schema = "financas")
-@Builder
-@Data
+@Builder(toBuilder = true)
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+@Setter(value = AccessLevel.PACKAGE)
+@Getter
 public class Usuario {
-	
+
 	@Id
 	@Column(name = "id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
